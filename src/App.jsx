3b74bd8b21d4ecd11f1c7a6b629b20214a1065c6
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-12 20:33:31 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const [logic, setLogic] = useState(0);
+  console.log(index);
+  console.log(setIndex);
+
   return (
     <div className="body">
       <div className="container">
@@ -11,7 +17,7 @@ const App = () => {
         </div>
         <div className="value">0</div>
         <div className="main">
-          <div className="next main-btn">
+          <div onClick={} className="next main-btn">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -20,7 +26,7 @@ const App = () => {
               <path d="M11.9999 10.8284L7.0502 15.7782L5.63599 14.364L11.9999 8L18.3639 14.364L16.9497 15.7782L11.9999 10.8284Z"></path>
             </svg>
           </div>
-          <div className="back main-btn">
+          <div onClick={} className="back main-btn">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
