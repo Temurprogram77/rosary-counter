7bba651e1b7f646e76d2af3065509ec559215682
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-13 11:10:27 +0300

    added

--- src/App.jsx
+++ src/App.jsx
@@ -52,12 +52,21 @@ const App = () => {
       <div className="container">
         <div className="btns">
           <select className="button1">
-            <option value="1">+1</option>
-            <option value="2">+2</option>
-            <option value="3">+3</option>
-            <option value="4">+4</option>
-            <option value="5">+5</option>
+            <option value="100">+100</option>
+            <option value="50">+50</option>
+            <option value="30">+30</option>
+            <option value="30">+30</option>
+            <option value="15">+15</option>
+            <option value="10">+10</option>
+            <option value="9">+9</option>
+            <option value="8">+8</option>
+            <option value="7">+7</option>
             <option value="6">+6</option>
+            <option value="5">+5</option>
+            <option value="4">+4</option>
+            <option value="3">+3</option>
+            <option value="2">+2</option>
+            <option value="1">+1</option>
           </select>
 
           <div className="button2">Edit</div>
